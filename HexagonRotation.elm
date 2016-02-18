98184b04e2637eccf61d87633c725bc355bf0293
Martin Chabot <chabotm@gmail.com>
2016-02-17 19:30:09 -0500

    formatting

--- HexagonRotation.elm
+++ HexagonRotation.elm
@@ -1,31 +1,43 @@
+module Main (..) where
+
 import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 import Mouse
 
-mouse = Mouse.position
 
-angle : (Int, Int) -> Int
-angle (x, y) = x % 360
+mouse =
+  Mouse.position
+
+
+angle : ( Int, Int ) -> Int
+angle ( x, y ) =
+  x % 360
+
 
 main : Signal Element
 main =
-    Signal.map action mouse
-        
-action (x,y) = 
-    collage 300 300
-        [polygons (toFloat (angle (x,y)))]
+  mouse |> Signal.map action
 
+action : ( Int, Int ) -> Element
+action ( x, y ) =
+  collage
+    300
+    300
+    [ polygons (toFloat (angle ( x, y ))) ]
+
+polygons: Float -> Form 
 polygons angle =
-  group [ 
-    hexagon red red, 
-    hexagon purple purple 
-      |> rotate (degrees angle)
-      |> scale 2, 
-    hexagon red red|> rotate (degrees -angle)]
+  group
+    [ hexagon red red
+    , hexagon purple purple
+        |> rotate (degrees angle)
+        |> scale 2
+    , hexagon red red |> rotate (degrees -angle)
+    ]
 
-    
 
 hexagon : Color -> Color -> Form
-hexagon clr innerClr=
-    ngon 6 40 |> filled innerClr
+hexagon clr innerClr =
+  ngon 6 40 |> filled innerClr
+
